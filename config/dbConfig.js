732280b4aeb53573c91a656dc0b689fea8668d31
Quinten Becker <qbecker@asu.edu>
2016-12-03 21:24:12 +0000

    testing package.json

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,11 +1,4 @@
-const net = require('net');
-const client = net.connect({port: 80, host:"google.com"}, () => {
-  console.log('MyIP='+client.localAddress);
-  console.log('MyPORT='+client.localPort);
-  return client.localAddress;
-});
 
-
-var url = "mongodb://"+ client+":27017/my_database_name";
+var url = "mongodb://104.131.52.79:27017/my_database_name";
 
 module.exports.url = url;
